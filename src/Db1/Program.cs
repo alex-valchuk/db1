1343d857e95bc09d9ad1239b0cf6b63fb32e3a28
Alex Valchuk <alex.valchuk@deltatre.it>
2021-08-03 16:34:04 +0300

    Create skeleton

---
+++ src/Db1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Db1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
